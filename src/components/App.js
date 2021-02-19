bd1aab71ee14c9d0300ee5d37aa8df0346c82afb
Glen-Taylor37 <gtaylor37@gmail.com>
2021-02-19 15:47:42 -0500

    Added Title and styling for Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Header from './Header';
+import Title from './Title';
 import JournalList from './journals/JournalList';
 
 const App = () => {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import Title from './Title';
 
 const Header = () => {
-	return <button>Sign in</button>;
+	return (
+		<div className="header">
+			<Title />
+			<div className="nav">
+				<button className="ui button">Journals</button>
+				<div className="nav-right">
+					<button className="ui button">Settings</button>
+					<button className="ui button">Sign in</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default connect(null, {})(Header);
