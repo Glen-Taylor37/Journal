6900b0b6bb94ff259e8ca82f4aeb3242afbd0686
Glen-Taylor37 <gtaylor37@gmail.com>
2021-02-19 15:56:48 -0500

    More icons for buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,12 @@ const Header = () => {
 			<div className="nav">
 				<button className="ui button">Journals</button>
 				<div className="nav-right">
-					<button className="ui button">Settings</button>
-					<button className="ui button">Sign in</button>
+					<button className="ui button">
+						<i className="fas fa-cog" />Settings
+					</button>
+					<button className="ui button">
+						<i className="fab fa-google" /> Sign in
+					</button>
 				</div>
 			</div>
 		</div>
