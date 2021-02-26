c594c5338986407f878d4621ec082439d5266ae7
Glen <gtaylor37@gmail.com>
2021-02-26 00:25:40 -0500

    Add Quill text editor to Journal page

--- src/components/journals/JournalShow.js
+++ src/components/journals/JournalShow.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import Quill from 'quill';
 import ContentDiv from '../shared/ContentDiv';
-
 import { getJournals } from '../../actions';
 
 const JournalDiv = styled.div``;
+const EditorDiv = styled.div`
+	width: 100%;
+	height: 100%;
+`;
 
 class JournalShow extends React.Component {
+	constructor(props) {
+		super(props);
+		this.editor = null;
+	}
+
+	componentDidMount() {
+		this.props.getJournals();
+	}
+
+	componentDidUpdate() {
+		this.addEditor();
+	}
+
+	addEditor() {
+		if (!this.editor) {
+			this.editorRoot = document.querySelector('#editor');
+			const options = {
+				debug       : 'info',
+				placeholder : 'Compose an epic...',
+				readOnly    : false,
+				theme       : 'snow',
+				bounds      : this.editorRoot
+			};
+			this.editor = new Quill(this.editorRoot, options);
+		}
+	}
+
 	render() {
 		if (!this.props.journal) {
 			return null;
@@ -15,7 +46,10 @@ class JournalShow extends React.Component {
 
 		return (
 			<ContentDiv>
-				<JournalDiv>{this.props.journal.title}</JournalDiv>
+				<JournalDiv>
+					{this.props.journal.title}
+					<EditorDiv id="editor" />
+				</JournalDiv>
 			</ContentDiv>
 		);
 	}
